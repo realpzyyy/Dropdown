e0b09d76ea52360e1bd883585fb563402d403e0a
realpzyyy <121077552@qq.com>
2018-03-08 23:48:09 +0800

    为下拉框配置属性增加默认值

--- components/dropDown.js
+++ components/dropDown.js
@@ -8,8 +8,8 @@ Vue.component('drop-down', {
     props: ['contents'],
     template:
     '<div>'+
-        '<el-dropdown v-for="(content,index) in contents" :key="index" :trigger="content.configs.trigger" :placement="content.configs.placement" style="padding-right: 30px">'+
-          '<el-button :type="content.configs.type" :size="content.configs.size">'+
+        '<el-dropdown v-for="(content,index) in contents" :key="index" :trigger="uesDefaultConfigs(content.configs).trigger" :placement="uesDefaultConfigs(content.configs).placement" style="padding-right: 30px">'+
+          '<el-button :type="uesDefaultConfigs(content.configs).type" :size="uesDefaultConfigs(content.configs).size">'+
             '{{content.title}}<i class="el-icon-arrow-down el-icon--right"></i>'+
           '</el-button>'+
           '<el-dropdown-menu slot="dropdown">'+
@@ -19,15 +19,19 @@ Vue.component('drop-down', {
     '</div>',
     data: function () {
             return {
-                //defaultConfigs : dropdownComponents,
+                defaultConfigs : {
+                    'type'          : '',  
+                    'size'          : 'medium', 
+                    'trigger'       : 'hover',
+                    'placement'     : 'bottom-end'
+                },
             }
         },
-    computed:{
-            defaultConfigs:function(){
-                return primaryChoices.filter(function(primaryChoice){
-                    
-                });
-            },
-        }
+    
+    methods:{
+            uesDefaultConfigs  :function(configs){
+                return configs ? configs : this.defaultConfigs
+            }
+        },
 
 })
